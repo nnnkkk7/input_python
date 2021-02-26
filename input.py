353e2298a03be7d81dec7cb0b83abb6bf9fda1d1
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-02-26 10:19:03 +0900

    first commit

---
+++ input.py
@@ -0,0 +1,8 @@
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+person = input("person: ")
+
+input_str = f"computer programming is {adj}! It makes me so excited because I love to {verb1}. I try to {verb2} like you are {person}!"
+
+print(input_str)
